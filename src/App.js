70e3f64739bbc2e9a610d7b60e426aabd6f0f698
Samriddhi <agrawalsamriddhi83@gmail.com>
2022-11-02 14:34:31 +0530

    feat: add home page layout along with header, main, footer and set favicon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='app'>
+        {/* <header className='App-header'>
+        </header> */}
+        <main id='main-content' className='main-content'>
+          <section className='top-section'>
+            <div className='feature-list'>
+              <p><centre>Hello World!</centre></p>
+            </div>
+            <div className='description'>
+            <p><centre>Hello Kittty!</centre></p>
+            </div>
+          </section>
+          <section className='middle-section'>
+            <p>Hello kitty, it's wednesday!</p>
+          </section>
+        </main>
+        <hr></hr>
+        <footer className='footer'>
+          Made with  ❤️  by the AsyncAPI Initiative.
+          {/* Copyright © AsyncAPI Project a Series of LF Projects, LLC. For web site terms of use, <br></br>
+          trademark policy and general project policies please see https://lfprojects.org */}
+        </footer>
+      </div>
+    </>
   );
 }
 
