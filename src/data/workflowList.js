4ade3c999f3cf19bd3febed6b953e597c22b79f2
Samriddhi <agrawalsamriddhi83@gmail.com>
2022-11-04 12:21:02 +0530

    chore: add workflow in the json

--- src/data/workflowList.js
+++ src/data/workflowList.js
@@ -1,62 +1,124 @@
 export const workflowList = [
     {
         id: 1,
-        title: 'Add good first issue'
+        title: 'Add good first issue',
+        name: 'add-good-first-issue-labels'
     },
     {
         id: 2,
-        title: 'Add good first issue'
+        title: 'Automerge for humans to add label',
+        name: 'automerge-for-humans-add-ready-to-merge-or-do-not-merge-label'
     },
     {
         id: 3,
-        title: 'Add good first issue'
+        title: 'Automerge for humans merging',
+        name: 'automerge-for-humans-merging'
     },
     {
         id: 4,
-        title: 'Add good first issue'
+        title: 'Automerge for humans to remove label',
+        name: 'automerge-for-humans-remove-ready-to-merge-label-on-edit'
     },
     {
         id: 5,
-        title: 'Add good first issue'
+        title: 'Automerge Orphans',
+        name: 'automerge-orphans'
     },
     {
         id: 6,
-        title: 'Add good first issue'
+        title: 'Automerge',
+        name: 'automerge'
     },
     {
         id: 7,
-        title: 'Add good first issue'
+        title: 'Autoupdate',
+        name: 'autoupdate'
     },
     {
         id: 8,
-        title: 'Add good first issue'
+        title: 'Bump',
+        name: 'bump'
     },
     {
         id: 9,
-        title: 'Add good first issue'
+        title: 'Global replicator',
+        name: 'global-replicator'
     },
     {
         id: 10,
-        title: 'Add good first issue'
+        title: 'Help Command',
+        name: 'help-command'
     },
     {
         id: 11,
-        title: 'Add good first issue'
+        title: 'If docker PR testing',
+        name: 'if-docker-pr-testing'
     },
     {
         id: 12,
-        title: 'Add good first issue'
+        title: 'If go PR testing',
+        name: 'if-go-pr-testing'
     },
     {
         id: 13,
-        title: 'Add good first issue'
+        title: 'If nodejs PR testing',
+        name: 'if-nodejs-pr-testing'
     },
     {
         id: 14,
-        title: 'Add good first issue'
+        title: 'If nodejs release',
+        name: 'if-nodejs-release'
     },
     {
         id: 15,
-        title: 'Add good first issue'
+        title: 'If nodejs version bump',
+        name: 'if-nodejs-version-bump'
     },
+    {
+        id: 16,
+        title: 'Issues PRs notifications',
+        name: 'issues-prs-notifications'
+    },
+    {
+        id: 17,
+        title: 'Link check cron',
+        name: 'link-check-cron'
+    },
+    {
+        id: 18,
+        title: 'Link check pr',
+        name: 'link-check-pr'
+    },
+    {
+        id: 19,
+        title: 'Lint pr title',
+        name: 'lint-pr-title'
+    },
+    {
+        id: 20,
+        title: 'Notify TSC members',
+        name: 'notify-tsc-members-mention'
+    },
+    {
+        id: 21,
+        title: 'Release Announcements',
+        name: 'release-announcements'
+    },
+    {
+        id: 22,
+        title: 'Sentiment Analysis',
+        name: 'sentiment-analysis'
+    },
+    {
+        id: 23,
+        title: 'Stale issues PRs',
+        name: 'stale-issues-prs'
+    },
+    {
+        id: 24,
+        title: 'Welcome first time contrib',
+        name: 'welcome-first-time-contrib'
+    },
+
+
 ]
