7a76dca63004029c9aa8cbe2b2b4e7685d3be3c3
Samriddhi <agrawalsamriddhi83@gmail.com>
2022-11-09 13:46:42 +0530

    add. responsiveness to ui

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,10 +4,23 @@ function Footer() {
     return (
         <>
             <p>
-                Made with  ❤️  by the AsyncAPI Initiative.
+                {/* Made with  ❤️  by the AsyncAPI Initiative. */}
                 {/* Copyright © AsyncAPI Project a Series of LF Projects, LLC. For web site terms of use, <br></br>
                     trademark policy and general project policies please see https://lfprojects.org */}
             </p>
+            <nav className='footer-nav' aria-label="footer">
+                <span>Copyright © AsyncAPI Project</span>
+                <ul className='nav-list'>
+                    {/* <li className='meta-text'>Copyright © AsyncAPI Project</li> */}
+                    <li><a href='/'>Twitter</a></li>
+                    <li><a href='/'>GitHub</a></li>
+                    <li><a href='/'>LinkedIn</a></li>
+                    <li><a href='/'>YouTube</a></li>
+                    <li><a href='/'>Slack</a></li>
+                    <li><a href='/'>Twitch</a></li>
+                    <li><a href="mailto:press@asyncapi.io">Email Us</a></li>
+                </ul>
+            </nav>
         </>
     )
 }
