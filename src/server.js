342c110eba2d309a6929fe1ee1b7acecac8a79a4
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2023-01-06 22:37:08 +0530

    refactor. the logic to get the response of workflow runs

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'node:url';
 import { Low } from 'lowdb';
 import { JSONFile } from 'lowdb/node';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -18,56 +19,60 @@ const repositoryAdapter = new JSONFile(repositoryFile);
 const repositoryDB = new Low(repositoryAdapter);
 repositoryDB.data ||= { repositoryList: [] };
 
-
-// const octokit = new Octokit({
-//     auth: {
-//         clientId: process.env.CLIENT_ID,
-//         clientSecret: process.env.CLIENT_SECRET
-//     }
-// });
-
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN
 });
 
-// console.log("secret id: ", process.env.CLIENT_ID, " : ", process.env.CLIENT_SECRET);
 
 async function fetchRepositories() {
+
     const { data } = await octokit.rest.repos.listForOrg({
         org: "asyncapi",
         per_page: 100
     })
-    // console.log(data);
+
     repositoryDB.data.repositoryList = data.map(repository => repository.name);
+
     await repositoryDB.write();
     await fetchWorkflowData();
 }
 
 async function fetchWorkflowData() {
+
     const repositories = repositoryDB.data.repositoryList;
-    console.log(repositories.length);
+
     for (let index = 0; index < repositories.length; index++) {
         const { data } = await octokit.rest.actions.listWorkflowRunsForRepo({
             owner: "asyncapi",
             repo: repositories[index],
             per_page: 100
         })
-        workflowDB.data.workflowRuns.push(data.workflow_runs);
+
+        // eslint-disable-next-line array-callback-return
+        data.workflow_runs.map(run => {
+            let workflow = {
+                "id": run.id,
+                "name": run.name,
+                "repository_name": run.repository.name,
+                "run_number": run.run_number,
+                "event": run.event,
+                "status": run.status,
+                "conclusion": run.conclusion,
+                "workflow_id": run.workflow_id,
+                "created_at": run.created_at,
+                "updated_at": run.updated_at,
+                "run_started_at": run.run_started_at,
+            }
+
+            console.log("-------------------------------");
+            console.log(workflow);
+            console.log("-------------------------------");
+
+            workflowDB.data.workflowRuns.push(workflow);
+        })
+
         await workflowDB.write();
-        console.log("-------------------------------");
-        console.log(index, " -> ", data);
-        console.log("-------------------------------");
     }
-    // repositories.map(async ( repo, index )=> {
-    //     const { data } = await octokit.rest.actions.listWorkflowRunsForRepo({
-    //         owner: "asyncapi",
-    //         repo: repo,
-    //         per_page: 100
-    //     })
-    //     workflowDB.data.workflowRuns = data.workflow_runs;
-    //     await workflowDB.write();
-    //     console.log(index, " -> ", data);
-    // });
 }
 
 fetchRepositories();
