e3c7d1654f0d25d0af813066f21e5b11a1ea5cce
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2023-01-24 13:32:18 +0530

    refactor. statistic logic

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,7 +1,38 @@
 import './Statistics.css';
+import { useContext } from 'react';
 import PieGraph from '../graph/PieGraph';
+import { AppContext } from '../../context/AppContext.js';
+import workflow from '../../workflowRuns.json';
 
 function Statistics() {
+
+    const { state } = useContext(AppContext);
+    const repo = state.repoList.slice(-1)[0];
+
+    const renderSpecificRepositoryStats = () => {
+
+        const workflowList = workflow.list.filter(data => data.repository_name === repo);
+
+        let data = [];
+
+        for (const run of workflowList) {
+
+            const createdAtTime = Date.parse(run.created_at);
+            const updatedAtTime = Date.parse(run.updated_at);
+            const duration = Math.floor(((updatedAtTime - createdAtTime) / 1000) % 60);
+
+            let maxDurationRun = {
+                'name': run.name,
+                'duration': duration
+            }
+
+            data.push(maxDurationRun);
+        }
+        return data.reduce((data1, data2) => data1.duration > data2.duration ? data1 : data2);
+    }
+
+    renderSpecificRepositoryStats();
+
     return (
         <>
             <div className='stats'>
--- src/helper/Helper.js
+++ src/helper/Helper.js
@@ -1,18 +1,19 @@
 import workflow from '../workflowRuns.json';
 
 export const workflowCountList = () => {
+
     let list = [];
 
     for (const data of workflow.list) {
 
         const createdAtTime = Date.parse(data.created_at);
         const updatedAtTime = Date.parse(data.updated_at);
-        const durationMs = Math.floor(((updatedAtTime - createdAtTime) / 1000 / 60) << 0);
+        const durationMin = Math.floor(((updatedAtTime - createdAtTime) / 1000 / 60) % 60);
 
         let run = {
             "name": data.name,
             "frequency": 1,
-            "duration": durationMs
+            "duration": durationMin
         };
 
         const pair = list.find(workflow => workflow.name === data.name);
@@ -23,7 +24,7 @@ export const workflowCountList = () => {
         }
         else {
             list[index].frequency += 1;
-            list[index].duration += durationMs;
+            list[index].duration += durationMin;
         }
     }
 
@@ -35,11 +36,8 @@ export const filterWorkflowStats = () => {
         conclusion: {
             success: 0,
             failure: 0,
-            cancelled: 0,
             skipped: 0,
-            startup_failure: 0,
-            action_required: 0,
-            null: 0
+            action_required: 0
         },
         durations: {
             success: [],
@@ -49,7 +47,10 @@ export const filterWorkflowStats = () => {
     }
 
     for (const run of workflow.list) {
-        stats.conclusion[run.conclusion] += 1;
+
+        if (stats.conclusion[run.conclusion] !== undefined) {
+            stats.conclusion[run.conclusion] += 1;
+        }
         const createdAtTime = Date.parse(run.created_at);
         const updatedAtTime = Date.parse(run.updated_at);
         const durationMs = updatedAtTime - createdAtTime;
@@ -63,8 +64,6 @@ export const filterWorkflowStats = () => {
             stats.durations[run.conclusion].push(data);
         }
     }
-
-    // console.log(stats.durations.failure);
 
     return stats;
 }
