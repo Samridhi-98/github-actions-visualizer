7df66c7fdd252815e238e24b3fad8521894e599b
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2023-01-26 17:08:39 +0530

    add pagination logic to set workflow data for prev 10 days

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 import { Octokit } from "@octokit/rest";
 import { join, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -8,7 +9,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN
+    auth: process.env.GITHUB_TOKEN,
 });
 
 function createFile(filename) {
@@ -39,27 +40,23 @@ async function fetchRepositories() {
     await fetchWorkflowData();
 }
 
-async function fetchWorkflowData() {
-
-    const repositories = repository.data.list;
-
-    if (new Date().getDate() === 1) {
-        workflow.data.list = [];
-    }
-
-    for (let index = 0; index < repositories.length; index++) {
-        const { data } = await octokit.rest.actions.listWorkflowRunsForRepo({
-            owner: "asyncapi",
-            repo: repositories[index],
-            per_page: 100
-        })
-        const currentMonth = new Date().getMonth();
-        const currentYear = new Date().getFullYear();
-
-        // eslint-disable-next-line array-callback-return
-        data.workflow_runs.map(run => {
-            let isSameYearAndMonth = (new Date(run.created_at).getFullYear() === currentYear) && (new Date(run.created_at).getMonth() === currentMonth);
-            if (isSameYearAndMonth) {
+// eslint-disable-next-line no-unused-vars
+function setDataToJson(repository, prevTenDays) {
+    octokit
+        .paginate(
+            "GET /repos/{owner}/{repo}/actions/runs",
+            { owner: "asyncapi", repo: repository, per_page: "100" },
+            (response, done) => {
+                let condition = response.data.find(workflow => new Date(workflow.created_at).toLocaleDateString() === prevTenDays);
+                if (condition) {
+                    done();
+                }
+                console.log(repository);
+                return response.data
+            }
+        )
+        .then((workflowData) => {
+            workflowData.map((run) => {
                 let data = {
                     "id": run.id,
                     "name": run.name,
@@ -73,30 +70,27 @@ async function fetchWorkflowData() {
                     "updated_at": run.updated_at,
                     "run_started_at": run.run_started_at,
                 }
+                // console.log(data)
                 if (workflow.data.list.find(run => run.id === data.id) === undefined) {
                     workflow.data.list.push(data);
                 }
-            }
-        })
+            })
+            workflow.write();
+        });
+}
 
-        await workflow.write();
+async function fetchWorkflowData() {
+    // const date = new Date();
+    // const prevMonthLastDate = new Date(date.getFullYear(), date.getMonth(), 0);
+    const repositories = repository.data.list;
+    const prevTenDays = new Date(new Date().setDate(new Date().getDate() - 10)).toLocaleDateString();
+    if (new Date().getDate() === 1) {
+        workflow.data.list = [];
+    }
+
+    for (let index = 0; index < repositories.length; index++) {
+        setTimeout(() => setDataToJson(repositories[index], prevTenDays), index * 5000);
     }
 }
 
-// fetchRepositories();
-
-// eslint-disable-next-line no-unused-vars
-async function fetchdata() {
-
-    // octokit
-    //     .paginate(
-    //         "GET /repos/{owner}/{repo}/actions/runs",
-    //         { owner: "asyncapi", repo: "tck", per_page: "100" },
-    //         (response) => response.data.map((workflow) => workflow.display_title)
-    //     )
-    //     .then((workflow) => {
-    //         console.log(workflow)
-    //     });
-    console.log("hello");
-}
-fetchdata();
+fetchRepositories();
